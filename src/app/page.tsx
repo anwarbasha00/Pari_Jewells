cdb8293efe158c7b8ffe18f3ed935fae9510162e
anwarbasha00 <anwarbashashaik983@gmail.com>
2026-07-13 10:28:27 +0530

    First commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className='text-5xl p-8 text-center'>welcome to pari jewelles!....</h1>
+    </div>
+  )
+}
+
+export default page
---
+++ src/lib/connectdb.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const MONGODB_URL = process.env.MONGODB_URL!;
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = {
+    conn: null,
+    promise: null,
+  };
+}
+
+export const connectDB = async () => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(MONGODB_URL)
+      .then((m) => m.connection);
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (error) {
+    cached.promise = null;
+    throw error;
+  }
+
+  return cached.conn;
+};
